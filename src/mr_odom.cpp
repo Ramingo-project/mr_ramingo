dc66859d6b17cf5e05b9020eb7b0851f835cacf5
GiuQua9 <giuseppe98.quaranta@gmail.com>
2023-05-11 13:22:47 +0200

    odom finished

--- src/mr_odom.cpp
+++ src/mr_odom.cpp
@@ -39,12 +39,14 @@ ROS_SUB::ROS_SUB() {
 void ROS_SUB::topic_cb( geometry_msgs::Twist cmd_vel) {
 
 	_cmd_vel = cmd_vel;
+
 }
 
 void ROS_SUB::odometry(){
 
     tf::TransformBroadcaster odom_broadcaster;
 	tf::Transform transform;
+	tf::Quaternion q;
    
     double x = 0.0;
     double y = 0.0;
@@ -54,51 +56,24 @@ void ROS_SUB::odometry(){
     double vy = 0.0;
     double vth = 0.1;
    
-    ros::Time current_time, last_time;
-    current_time = ros::Time::now();
-	last_time = ros::Time::now();
-   
     ros::Rate r(100);
     while(ros::ok()){
    
 		ros::spinOnce();               // check for incoming messages
-		current_time = ros::Time::now();
 		vx = _cmd_vel.linear.x;
 		vth = _cmd_vel.angular.z;
 		
 		//compute odometry in a typical way given the velocities of the robot
-		//double dt = (current_time - last_time).toSec();
-		double delta_x = (vx * cos(th))*0.01;// * dt;
-		double delta_y = (vx * sin(th))*0.01;// * dt;
-		double delta_th = vth *0.01;//* dt;
-	
-		x += delta_x;
-		y += delta_y;
-		th += delta_th;
-	
-		//since all odometry is 6DOF we'll need a quaternion created from yaw
-		tf::Quaternion q;
-		
+		x += (vx * cos(th))*0.01;
+		y += (vx * sin(th))*0.01;
+		th += vth *0.01;		
 	
 		//first, we'll publish the transform over tf
-		geometry_msgs::TransformStamped odom_trans;
-		odom_trans.header.stamp = current_time;
-		odom_trans.header.frame_id = "mr_odom";
-		odom_trans.child_frame_id = "base_link";
-	
-		odom_trans.transform.translation.x = x;
-		odom_trans.transform.translation.y = y;
-		odom_trans.transform.translation.z = 0.0;
-		//odom_trans.transform.rotation = odom_quat;
-	
-		//send the transform
-		//odom_broadcaster.sendTransform(odom_trans);
-
-
 		transform.setOrigin(tf::Vector3(x, y, 0.0));
         q.setRPY(0, 0, th);
 		transform.setRotation(q);
 
+		//send the transform
 		odom_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "mr_odom", "base_link"));
 	
 		//next, we'll publish the odometry message over ROS
@@ -121,7 +96,6 @@ void ROS_SUB::odometry(){
 		//publish the message
 		odom_pub.publish(odom);*/
 	
-		last_time = current_time;
 		r.sleep();
    }
 }
