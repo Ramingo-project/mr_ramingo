35b0ba993176e97f9cead88ca220fac706495a2f
GiuQua9 <giuseppe98.quaranta@gmail.com>
2023-05-11 12:29:42 +0200

    odom finished

--- src/mr_odom.cpp
+++ src/mr_odom.cpp
@@ -44,6 +44,7 @@ void ROS_SUB::topic_cb( geometry_msgs::Twist cmd_vel) {
 void ROS_SUB::odometry(){
 
     tf::TransformBroadcaster odom_broadcaster;
+	tf::Transform transform;
    
     double x = 0.0;
     double y = 0.0;
@@ -57,51 +58,59 @@ void ROS_SUB::odometry(){
     current_time = ros::Time::now();
 	last_time = ros::Time::now();
    
-    ros::Rate r(1.0);
+    ros::Rate r(100);
     while(ros::ok()){
    
 		ros::spinOnce();               // check for incoming messages
 		current_time = ros::Time::now();
-		vx = _cmd_vel.linear.x*r_wheel;
+		vx = _cmd_vel.linear.x;
 		vth = _cmd_vel.angular.z;
 		
 		//compute odometry in a typical way given the velocities of the robot
-		double dt = (current_time - last_time).toSec();
-		double delta_x = (vx * cos(th)) * dt;
-		double delta_y = (vx * sin(th)) * dt;
-		double delta_th = vth * dt;
+		//double dt = (current_time - last_time).toSec();
+		double delta_x = (vx * cos(th))*0.01;// * dt;
+		double delta_y = (vx * sin(th))*0.01;// * dt;
+		double delta_th = vth *0.01;//* dt;
 	
 		x += delta_x;
 		y += delta_y;
 		th += delta_th;
 	
 		//since all odometry is 6DOF we'll need a quaternion created from yaw
-		geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
+		tf::Quaternion q;
+		
 	
 		//first, we'll publish the transform over tf
 		geometry_msgs::TransformStamped odom_trans;
 		odom_trans.header.stamp = current_time;
-		odom_trans.header.frame_id = "odom";
+		odom_trans.header.frame_id = "mr_odom";
 		odom_trans.child_frame_id = "base_link";
 	
 		odom_trans.transform.translation.x = x;
 		odom_trans.transform.translation.y = y;
 		odom_trans.transform.translation.z = 0.0;
-		odom_trans.transform.rotation = odom_quat;
+		//odom_trans.transform.rotation = odom_quat;
 	
 		//send the transform
-		odom_broadcaster.sendTransform(odom_trans);
+		//odom_broadcaster.sendTransform(odom_trans);
+
+
+		transform.setOrigin(tf::Vector3(x, y, 0.0));
+        q.setRPY(0, 0, th);
+		transform.setRotation(q);
+
+		odom_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "mr_odom", "base_link"));
 	
 		//next, we'll publish the odometry message over ROS
-		nav_msgs::Odometry odom;
+		/*nav_msgs::Odometry odom;
 		odom.header.stamp = current_time;
-		odom.header.frame_id = "odom";
+		odom.header.frame_id = "mr_odom";
 	
 		//set the position
 		odom.pose.pose.position.x = x;
 		odom.pose.pose.position.y = y;
 		odom.pose.pose.position.z = 0.0;
-		odom.pose.pose.orientation = odom_quat;
+		odom.pose.pose.orientation = q;
 	
 		//set the velocity
 		odom.child_frame_id = "base_link";
@@ -110,7 +119,7 @@ void ROS_SUB::odometry(){
 		odom.twist.twist.angular.z = vth;
 	
 		//publish the message
-		odom_pub.publish(odom);
+		odom_pub.publish(odom);*/
 	
 		last_time = current_time;
 		r.sleep();
