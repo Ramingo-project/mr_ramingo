128a2a007319eb2fbd71fbef75bf6a50fad7b95a
melaniamariniello <melaniamariniello15@gmail.com>
2023-06-14 20:38:26 +0200

    joystick clean

--- src/joystick.cpp
+++ src/joystick.cpp
@@ -1,8 +1,8 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
-#include "std_msgs/Int32.h"
-#include "std_msgs/Float32.h"
 #include "geometry_msgs/Twist.h"
+#include "boost/thread.hpp"
+
 
 using namespace std;
 
@@ -10,30 +10,42 @@ class ROS_SUB {
 	public:
 		ROS_SUB();
 		void topic_cb( sensor_msgs::Joy data);
-	
+		void joystick();
+
 	private:
 		ros::NodeHandle _nh;
-		//Subscriber object
+		//Subscriber and Publisher object
 		ros::Subscriber joystick_sub;
 		ros::Publisher joystick_pub;
-        float acc;
-        float steer;
+		float x;
+		float z;
 };
 
 ROS_SUB::ROS_SUB() {
 	//Initialize a subscriber and a publisher
 	joystick_sub = _nh.subscribe("/joy", 1, &ROS_SUB::topic_cb, this);
-	joystick_pub = _nh.advertise<geometry_msgs::Twist>("/mr_ramingo/cmd_vel", 10);
+	joystick_pub = _nh.advertise<geometry_msgs::Twist>("/dynamixel_workbench/cmd_vel", 10);
+	boost::thread loop_t(&ROS_SUB::joystick, this); 
 }
 
 void ROS_SUB::topic_cb( sensor_msgs::Joy data) {
-	geometry_msgs::Twist cmd;
-    cmd.linear.x = data.axes[1];
-    cmd.angular.z = data.axes[2];
-	joystick_pub.publish(cmd);
+    x = data.axes[1];
+    z = data.axes[2];
 }
 
+void ROS_SUB::joystick(){
 
+	ros::Rate rate(1000);
+	geometry_msgs::Twist cmd;
+	while(ros::ok()){
+		cmd.linear.x = x;
+		cmd.angular.z = z;
+		joystick_pub.publish(cmd);
+		rate.sleep();
+	}
+	
+
+}
 int main( int argc, char** argv ) {
 
 	//Init the ros node with  name
