d688e59c9ed717744e424444a1f239cf32f447aa
melaniamariniello <melaniamariniello15@gmail.com>
2023-06-14 20:09:55 +0200

    mr_goal_pos clean

--- src/mr_goal_pos.cpp
+++ src/mr_goal_pos.cpp
@@ -1,7 +1,4 @@
 #include "ros/ros.h"
-#include "std_msgs/Int32.h"
-#include "std_msgs/Float32.h"
-#include "boost/thread.hpp"
 #include <tf/transform_listener.h>
 #include <actionlib/client/simple_action_client.h>
 #include <move_base_msgs/MoveBaseAction.h>
@@ -11,7 +8,12 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 
 int main( int argc, char** argv ) {
     ros::init(argc, argv, "move_base_client");
-    ros::NodeHandle nh;
+
+    move_base_msgs::MoveBaseGoal goal;
+    tf::TransformListener listener;
+    tf::StampedTransform transform;
+    float k = 0.3;
+    bool detected = false;
 
     //tell the action client that we want to spin a thread by default
     MoveBaseClient ac("move_base", true);
@@ -21,42 +23,51 @@ int main( int argc, char** argv ) {
         ROS_INFO("Waiting for the move_base action server to come up");
     }
 
-    move_base_msgs::MoveBaseGoal goal;
-    tf::TransformListener listener;
-    tf::StampedTransform transform;
-    float k=0.3;
-    bool detected = false;
     while(!detected){
 
-        goal.target_pose.header.frame_id = "base_link";
-        goal.target_pose.header.stamp = ros::Time::now();
         goal.target_pose.header.frame_id = "base_link";
         goal.target_pose.header.stamp = ros::Time::now();
         goal.target_pose.pose.position.x = 1*k;
         goal.target_pose.pose.position.y = ((rand()%3)-1)*k/2;
         goal.target_pose.pose.position.z = 0.0;
         goal.target_pose.pose.orientation.w = 1.0;
-        ac.sendGoal(goal);
 
         try{
             listener.waitForTransform("base_link", "aruco_marker_frame", ros::Time(0), ros::Duration(1.0));
             listener.lookupTransform("base_link", "aruco_marker_frame", ros::Time(0), transform);
-            goal.target_pose.pose.position.x = transform.getOrigin().x()-0.3;
-            goal.target_pose.pose.position.y = transform.getOrigin().y()-0.3;
+            goal.target_pose.pose.position.x = 0.0;
+            goal.target_pose.pose.position.y = 0.0;
             goal.target_pose.pose.position.z = 0.0;
             goal.target_pose.pose.orientation.x = 0.0;
             goal.target_pose.pose.orientation.y = 0.0;
-            goal.target_pose.pose.orientation.z = transform.getRotation().z();
+            goal.target_pose.pose.orientation.z = 0.0;
             goal.target_pose.pose.orientation.w = 1.0;
-            ac.sendGoal(goal);
-            detected =true;
+            detected = true;
+            ros::Duration(2.0);
         }
         catch(tf::TransformException ex){
             ROS_ERROR("%s",ex.what());
 		    ros::Duration(0.2).sleep();
         }
+        //Send the goal
+        ac.sendGoal(goal);
     }  
 
+    ROS_INFO("research aruco_marker_frame");
+
+    // Retreive the marker position again and set the right goal position 
+    listener.waitForTransform("base_link", "aruco_marker_frame", ros::Time(0), ros::Duration(1.0));
+    listener.lookupTransform("base_link", "aruco_marker_frame", ros::Time(0), transform);
+    goal.target_pose.pose.position.x = transform.getOrigin().x();
+    goal.target_pose.pose.position.y = transform.getOrigin().y();
+    goal.target_pose.pose.position.z = 0.0;
+    goal.target_pose.pose.orientation.x = 0.0;
+    goal.target_pose.pose.orientation.y = 0.0;
+    goal.target_pose.pose.orientation.z = transform.getRotation().z();
+    goal.target_pose.pose.orientation.w = 1.0;
+    ac.sendGoal(goal);
+    
+    ros::Duration(1.0);
     ac.waitForResult();
 
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
